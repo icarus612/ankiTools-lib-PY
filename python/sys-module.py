74dea77ef812425bf71e3f438ad3c86bc0ad58c0
icarus64 <ellishogan95@gmail.com>
2023-03-17 20:15:48 -0400

    update

---
+++ python/sys-module.py
@@ -0,0 +1,6 @@
+sys.stdin | <b>Sys property</b> used to 
+sys.stdout | <b>Sys property</b> used to 
+sys.stderr | <b>Sys property</b> used to 
+sys.argv | <b>Sys property</b> used to 
+sys.path | <b>Sys property</b> used to 
+sys.modules | <b>Sys property</b> used to 
