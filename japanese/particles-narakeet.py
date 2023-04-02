b2aeb7706a7590e79c01613cf57b83651ec8c103
icarus64 <ellishogan95@gmail.com>
2023-04-01 23:45:39 -0400

    update

--- japanese/particles-narakeet.py
+++ japanese/particles-narakeet.py
@@ -10,6 +10,9 @@ import json
 import os
 from time import sleep
 
+			
+audio_path = 'audio/particles/narakeet'
+working_proxy = ''
 def get_proxy_list():
   options = webdriver.ChromeOptions()
   options.add_argument("start-maximized")
@@ -19,58 +22,85 @@ def get_proxy_list():
   driver.get("https://sslproxies.org/")
   proxies = [[td.get_attribute('innerHTML') for td in tr.find_elements(By.TAG_NAME, 'td')] for tr in driver.find_element(By.TAG_NAME, 'table').find_elements(By.TAG_NAME, 'tr')][1:]
   driver.quit()
-  return [':'.join(proxy[:2]) for proxy in proxies if proxy[4] == 'elite proxy']
+  return [':'.join(proxy[:2]) for proxy in proxies]
 
-#proxies = get_proxy_list()
-audio_path = 'audio/particles/narakeet'
-proxy_idx = 0
+def scrape_audio(proxy, val='wa'):
+	try:
+		options = webdriver.ChromeOptions()
+		options.add_argument(f'--proxy-server={proxy}')
+		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+		driver.get(f'https://www.narakeet.com/languages/japanese-text-to-speech/')
+	except Exception as e:
+		print(e)
+		return False
+
+	for idx in range(8):
+		sleep(2)
+		try:
+			format_select = Select(driver.find_element(By.ID, 'cfgAudioFormat'))
+			format_select.select_by_value('mp3')
+			voice_select = Select(driver.find_element(By.ID, 'cfgVideoVoice'))
+			voice_select.select_by_value('yuriko')
+			driver.find_element(By.CSS_SELECTOR, 'textarea.textarea').clear()
+			driver.find_element(By.CSS_SELECTOR, 'textarea.textarea').send_keys(val.split('/')[0])
+			driver.find_element(By.NAME, 'generateaudio').click()
+			break
+		except Exception as e:
+			if idx == 7:
+				print('Page load timeout.')
+				driver.close()
+				return False
+			
+	for idx in range(8):
+		try:
+			sleep(8)
+			status = driver.find_element(By.CSS_SELECTOR, '[data-show-stage]').get_attribute('stage').lower().strip()
+			print(status)
+			if status == 'finished':					
+				url = driver.find_element(By.CSS_SELECTOR, '[data-prop-link="result"]').get_attribute('href')
+				driver.close()
+				return url
+			elif status == 'error':
+				print('Audio creation error.')
+				driver.close()
+				return False
+		except:
+			if idx == 7:
+				print('Audio creation timeout.')
+				driver.close()
+				return False
+
+	# Just in case something somehow doesn't catch
+	driver.close()
+	return False
 
 try: 
 	os.mkdir(audio_path)
 except:
 	pass
 
-with open('./created-decks/jlptsensei-particles.json', 'r') as jlpt_json:
+for p in get_proxy_list():
+	print(f'Attempting to use proxy: {p}')
+	if scrape_audio(p):
+		print(f'Success! Setting proxy ip to {p}')
+		working_proxy = p
+		break
+	print('Moving on to next attempt.')
+
+with open('./created-decks/jlptsensei-particles-current.json', 'r') as jlpt_json:
 	raw_json = json.loads(jlpt_json.read())
 
 for deck_name, deck in raw_json.items():
 	with open(f'./jlptsensei-particles-{deck_name}.txt', 'w') as deck_file:
 		for card in deck:
 			file_name = f"ic_nrkt_{card[0].replace('...', '-').replace(' ', '').replace('/', '-')}.mp3"
-			#proxy = proxies[proxy_idx % len(proxies)]
-			#print(f'Proxy selected: {proxy}')
-			#options = webdriver.ChromeOptions()
-			#options.add_argument(f'--proxy-server={proxy}')
-			driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-			driver.get(f'https://www.narakeet.com/languages/japanese-text-to-speech/')
-			for _ in range(8):
-				sleep(1)
-				try:
-					format_select = Select(driver.find_element(By.ID, 'cfgAudioFormat'))
-					format_select.select_by_value('mp3')
-					voice_select = Select(driver.find_element(By.ID, 'cfgVideoVoice'))
-					voice_select.select_by_value('yuriko')
-					driver.find_element(By.CLASS_NAME, 'textarea').clear()
-					driver.find_element(By.CLASS_NAME, 'textarea').send_keys(card[1].split('/')[0])
-					driver.find_element(By.NAME, 'generateaudio').click()
-					break
-				except Exception as e:
-					print(e)
-					sleep(5)
+			audio_url = scrape_audio(working_proxy, card[1])
+			if audio_url: 
+				audio = requests.get(audio_url)
+				card.append(f'[sound:{file_name}]')
+				with open(os.path.join(audio_path, file_name), 'wb') as audio_file:
+					audio_file.write(audio.content)
 
-			for _ in range(8):
-				sleep(5)
-				status = driver.find_element(By.CSS_SELECTOR, '[data-show-stage]').get_attribute('stage').lower().strip()
-				print(status)
-				if status == 'finished':					
-					card.append(f'[sound:{file_name}]')
-					audio = requests.get(driver.find_element(By.CSS_SELECTOR, '[data-prop-link="result"]').get_attribute('href'))
-					with open(os.path.join(audio_path, file_name), 'wb') as audio_file:
-						audio_file.write(audio.content)
-					break
-				elif status == 'error':
-					raise Exception('Proxy Error')
 
-			driver.quit()
 			deck_file.write(' | '.join(card) + '\n')
 
