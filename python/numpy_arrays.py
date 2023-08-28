2198f0a31c39a9cdfc24d906e116a6ad0999bd5e
Ellis Hogan <36472242+icarus612@users.noreply.github.com>
2023-08-28 18:59:29 -0400

    minor update to get numpy

--- python/numpy_arrays.py
+++ python/numpy_arrays.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
+cards = []
 req = soup(requests.get('https://numpy.org/doc/stable/reference/arrays.ndarray.html').content, 'html.parser')
+
+urls = [i.href for i in req.find_all()]
+
+
+for url in urls:
+	item = soup(requests.get(url).content, 'html.parser')
+	front = item.find()
+	back = item.find()
+	card.append([front, back])
+	
+with open(f'numpy_arrays.txt', 'w') as file:
+	file.writelines([f'{" | ".join(i)} \n' for i in cards])
