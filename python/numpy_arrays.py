9f469f9cf6476677e85f682f6e6ec61dfebfae9d
icarus64 <ellishogan95@gmail.com>
2023-09-02 00:12:02 -0400

    nparray

--- python/numpy_arrays.py
+++ python/numpy_arrays.py
@@ -4,14 +4,21 @@ from bs4 import BeautifulSoup as soup
 cards = []
 req = soup(requests.get('https://numpy.org/doc/stable/reference/arrays.ndarray.html').content, 'html.parser')
 
-urls = [i.href for i in req.find_all()]
+urls = [i.find('a')["href"] for i in req.find_all(class_='toctree-l3')]
 
 
 for url in urls:
-	item = soup(requests.get(url).content, 'html.parser')
-	front = item.find()
-	back = item.find()
-	card.append([front, back])
-	
+	try:
+		print(f"Retrieving definition for {url.partition('.')[2][:-5]}")
+		full = soup(requests.get(f"https://numpy.org/doc/stable/reference/{url}").content, 'html.parser')
+		item = full.find('dl', class_="py")
+		full_m = item.find('dt', class_='sig-object')
+		front = f"ndarray.{full_m.find('span', {'sig-name'}).text}"
+		if 'method' in [i.text for i in full.find_all('p')]:
+			front += f"(<i>{''.join([i.text for i in full_m.find_all({'class': 'sig-param'})])}</i>)"
+		back = item.find('dd').find('p').text.replace('\n', '')
+		cards.append([front, back])
+	except Exception as e:
+		print(e)
 with open(f'numpy_arrays.txt', 'w') as file:
 	file.writelines([f'{" | ".join(i)} \n' for i in cards])
