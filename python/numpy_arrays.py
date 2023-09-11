8ce46fe7258e6653bd58d5672906dcfc0af08853
icarus64 <ellishogan95@gmail.com>
2023-09-11 18:04:14 -0400

    update

--- python/numpy_arrays.py
+++ python/numpy_arrays.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as soup
+import re
 
 cards = []
 req = soup(requests.get('https://numpy.org/doc/stable/reference/arrays.ndarray.html').content, 'html.parser')
@@ -9,15 +10,27 @@ urls = [i.find('a')["href"] for i in req.find_all(class_='toctree-l3')]
 
 for url in urls:
 	try:
-		print(f"Retrieving definition for {url.partition('.')[2][:-5]}")
+		url_p = url.partition('.')[2][:-5]
+		if re.search(r'__\w*__', url_p):
+			continue
+		print(f"Retrieving definition for {url_p}")
 		full = soup(requests.get(f"https://numpy.org/doc/stable/reference/{url}").content, 'html.parser')
 		item = full.find('dl', class_="py")
 		full_m = item.find('dt', class_='sig-object')
-		front = f"ndarray.{full_m.find('span', {'sig-name'}).text}"
+		front = f"<i>ndarray</i>.{full_m.find('span', {'sig-name'}).text}"
+		extra_info = ''
+		param_type = 'property'
 		if 'method' in [i.text for i in full.find_all('p')]:
-			front += f"(<i>{''.join([i.text for i in full_m.find_all({'class': 'sig-param'})])}</i>)"
-		back = item.find('dd').find('p').text.replace('\n', '')
-		cards.append([front, back])
+			param_type = 'method'
+			all_args = [i.text for i in full_m.find_all(class_='sig-param')]			
+			req_args = [x for x in all_args if '=' not in x]
+			if len(all_args) != len(req_args):
+				extra_info = front + f"(<i>{', '.join(all_args)}</i>)"
+			front += f"(<i>{', '.join(req_args)}</i>)"
+		
+		back = f"<b>Ndarray {param_type}</b> used to {item.find('dd').find('p').text}".replace('\n', '')
+		back = back[0].lower() + back[1:]
+		cards.append([front, back, '', extra_info])
 	except Exception as e:
 		print(e)
 with open(f'numpy_arrays.txt', 'w') as file:
